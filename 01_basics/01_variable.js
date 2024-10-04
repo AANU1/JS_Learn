6eb5389e9071dc8b2459655cc88dacf60f6d4b6c
Prasenjit Gaur <aanuthakur.1996@gmail.com>
2024-10-04 14:52:26 +0000

    datatypes

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,6 +1,6 @@
-console.log("PP")
 const accountId = 12
 let name = "Q"
 var phone = "1234"
 accountCity = "PAl"
+// becoz of block scope we will not use var .
 console.table([accountId, name,phone,accountCity])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,14 @@
+console.log(typeof undefined)  // output should be undefined
+console.log(typeof null)   // output is object
+// number==> 2 to the power 53
+//bigint
+//string==>""
+//boolean==>true
+//null==>standalone value
+//undefined==>
+ //   when we convert number
+//"33"==> 33
+//"33abc"==> NaN 
+
+let score = "33"
+console.log(typeof score)
